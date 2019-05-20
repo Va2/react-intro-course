58e8fdca448ad10c8a53672a216baf4a5d415c7e
Va2 <vadim.brieven@gmail.com>
2019-05-20 11:45:02 +0200

    Move Components into Separate Files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>John Doe</h1>
+            <p>Junior Web Developer</p>
+            <ol>
+                <li>ReactJS</li>
+                <li>Redux</li>
+                <li>React Native</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './style.css';
+import MyInfo from './components/MyInfo';
 
 import * as serviceWorker from './serviceWorker';
 
-function MyApp() {
-    return (
-        <div>
-            <h1>John Doe</h1>
-            <p>Junior Web Developer</p>
-            <ol>
-                <li>ReactJS</li>
-                <li>Redux</li>
-                <li>React Native</li>
-            </ol>
-        </div>
-    )
-}
-
-ReactDOM.render(<MyApp />, document.getElementById('root')
+ReactDOM.render(<MyInfo />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
