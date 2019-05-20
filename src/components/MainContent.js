045d77e4392210486613163fecfe6e53757dfdd6
Va2 <vadim.brieven@gmail.com>
2019-05-20 12:51:08 +0200

    JSX to JavaScript and Back - v3: CurrentTime

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import MyInfo from './MyInfo';
+import CurrentTime from './CurrentTime';
 
 function MainContent() {
     return (
         <main>
             <h3>Main Content</h3>
             <MyInfo />
+            <CurrentTime />
         </main>
     )
 }
