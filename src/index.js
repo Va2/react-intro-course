8fd23c77b96b2414c53c6aaf80553d7628433f3d
Va2 <vadim.brieven@gmail.com>
2019-05-20 11:26:46 +0200

    Functional Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './style.css';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <ul>
-        <li>ReactJS</li>
-        <li>Redux</li>
-        <li>React Native</li>
-    </ul>,
-    document.getElementById('root')
+function MyApp() {
+    return (
+        <div>
+            <h1>John Doe</h1>
+            <p>Junior Web Developer</p>
+            <ol>
+                <li>ReactJS</li>
+                <li>Redux</li>
+                <li>React Native</li>
+            </ol>
+        </div>
+    )
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
