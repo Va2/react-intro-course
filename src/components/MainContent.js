276b16157b7b9d4df06c4dcd363c5d1c8e5520d3
Va2 <vadim.brieven@gmail.com>
2019-05-20 15:54:00 +0200

    Props in React - v1

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import MyInfo from './MyInfo';
 import CurrentTime from './CurrentTime';
+import ContactCard from './ContactCard';
 
 function MainContent() {
     return (
@@ -9,6 +10,34 @@ function MainContent() {
             <h3>Main Content</h3>
             <MyInfo />
             <CurrentTime />
+
+            <ContactCard
+                name="John Doe"
+                imgUrl="http://lorempixel.com/500/280/animals/1"
+                phone="+32 2 226 53 53"
+                email="john@mail.com"
+            />
+
+            <ContactCard
+                name="Mary Pop"
+                imgUrl="http://lorempixel.com/500/280/animals/2"
+                phone="+32 2 226 45 99"
+                email="mary@mail.com"
+            />
+
+            <ContactCard
+                name="Jack Pixel"
+                imgUrl="http://lorempixel.com/500/280/animals/3"
+                phone="+32 2 226 33 88"
+                email="jack@mail.com"
+            />
+
+            <ContactCard
+                name="Betty Dol"
+                imgUrl="http://lorempixel.com/500/280/animals/4"
+                phone="+32 2 226 12 34"
+                email="betty@mail.com"
+            />
         </main>
     )
 }
