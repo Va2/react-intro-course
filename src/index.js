ef8df3c032127580f0a8abafe8b3c51b8260a3ba
Va2 <vadim.brieven@gmail.com>
2019-05-20 10:51:38 +0200

    ReactDOM & JSX

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ul>
+        <li>ReactJS</li>
+        <li>Redux</li>
+        <li>React Native</li>
+    </ul>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
