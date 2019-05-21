ad3d6b15fc799c6902d5d25c9509c8a645259cc3
Va2 <vadim.brieven@gmail.com>
2019-05-21 16:18:38 +0200

    State - v1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
-class App extends React.Component {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "John",
+            age: 25
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1>{this.props.whatever}</h1>
-
                 <Header />
                 <MainContent />
+
+                <div className="card">
+                    <h4>{this.state.name}</h4>
+                    <h4>{this.state.age} years old</h4>
+                </div>
+
                 <Footer />
             </div>
         )
