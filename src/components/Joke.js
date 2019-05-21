1a73f45aa85ccdd8f9ab58a576f4be5b08a8b466
Va2 <vadim.brieven@gmail.com>
2019-05-21 09:55:32 +0200

    Props in React - v4.1: conditional rendering other syntax

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Joke(props) {
     return (
        <div>
-            <h4 style={{display: props.question ? "block" : "none"}}>Question: {props.question}?</h4>
+            <h4 style={{display: !props.question && "none"}}>Question: {props.question}?</h4>
             <h4>Answer: {props.punchline}</h4>
        </div>
     )
