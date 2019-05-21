1dc680d68ac9df25a9dfadd005e985e739af1020
Va2 <vadim.brieven@gmail.com>
2019-05-21 16:32:03 +0200

    State - v2

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ class App extends Component {
         super()
         this.state = {
             name: "John",
-            age: 25
+            age: 25,
+            isLoggedIn: false
         }
     }
 
     render() {
+        // let wordDisplay = this.state.isLoggedIn === false ? "out" : "in";
+        let wordDisplay = this.state.isLoggedIn ? "in" : "out";
+
         return (
             <div>
                 <Header />
                 <MainContent />
 
                 <div className="card">
-                    <h4>{this.state.name}</h4>
-                    <h4>{this.state.age} years old</h4>
+                    <h4>{this.state.name} is {this.state.age} years old</h4>
+                    <h4>and is currently logged {wordDisplay}.</h4>
                 </div>
 
                 <Footer />
