7a32af73c9b18c2c11db4deaaf8789db576de7d7
Va2 <vadim.brieven@gmail.com>
2019-05-21 15:30:13 +0200

    Class-based Components - v2: cleanup

--- src/App.js
+++ src/App.js
@@ -4,24 +4,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
-// function App(props) {
-//     return (
-//         <div>
-//             <h1>{props.whatever}</h1>
-//             <Header />
-//             <MainContent />
-//             <Footer />
-//         </div>
-//     )
-// }
-
 class App extends React.Component {
-    // Declare methods inside the class
-
     render() {
-        // Running display logic (conditional rendering) and Style objects (inline styling) inside the render() method.
-
-
         return (
             <div>
                 <h1>{this.props.whatever}</h1>
