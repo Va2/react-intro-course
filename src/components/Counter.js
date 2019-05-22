0b1e312b1eee5dbcce3a046f653ae34d7585aba7
Va2 <vadim.brieven@gmail.com>
2019-05-22 11:12:36 +0200

    Changing State - v2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,27 +6,43 @@ class Counter extends React.Component {
         this.state = {
             count: 0
         }
-        this.handleClick = this.handleClick.bind(this)
+        this.plus1 = this.plus1.bind(this)
+        this.minus1 = this.minus1.bind(this)
+        this.devideBy2 = this.devideBy2.bind(this)
     }
     
-    handleClick() {
-        // Don't need to know previous state (OBJECT):
-        // this.setState({ count: 1 })
-
-        // Need to know previous state (pass Function):
+    plus1() {
         this.setState((prevState) => {
             return {
                 count: prevState.count + 1
             }
         })
     }
-    
+
+    minus1() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        })
+    }
+
+    devideBy2() {
+        this.setState((prevState) => {
+            return {
+                count: prevState.count / 2
+            }
+        })
+    }
+
     render() {
         return (
            <div className="card">
                 <h3>Counter</h3>
                 <h4>{this.state.count}</h4>
-                <button onClick={this.handleClick}>Change!</button>
+                <button onClick={this.plus1}>Plus 1</button>
+                <button onClick={this.minus1}>Minus 1</button>
+                <button onClick={this.devideBy2}>Devide by 2</button>
            </div>
         )
     }
