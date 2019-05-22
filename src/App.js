48672f40070bfcb569c54b64f0825d711f4da63b
Va2 <vadim.brieven@gmail.com>
2019-05-22 12:48:30 +0200

    Lifecycle Methods: Part 1

--- src/App.js
+++ src/App.js
@@ -14,6 +14,26 @@ class App extends Component {
         }
     }
 
+    componentDidMount() {
+        // GET the data I need to correctly display
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.whatever !== this.props.whatever) {
+            // do something important here
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // return true if want it to update
+        // return false if not
+    }
+
+    componentWillUnmount() {
+        // teardown or cleanup your code before your component disappears
+        // Ex: remove event listener
+    }
+
     render() {
         let wordDisplay = this.state.isLoggedIn ? "in" : "out";
 
