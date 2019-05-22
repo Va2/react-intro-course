aa4f58f8f9ba062395554e0cbe889d935423d6f1
Va2 <vadim.brieven@gmail.com>
2019-05-22 17:11:22 +0200

    Conditional rendering - Practice

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import Conditional from './components/Conditional';
+import LoginBtn from './components/LoginBtn';
 
 class App extends Component {
     constructor() {
@@ -12,8 +13,13 @@ class App extends Component {
             name: "John",
             age: 25,
             isLoggedIn: false,
-            isLoading: true
+            isLoading: true,
+            unreadMessages: [
+                "Learn ReactJS",
+                "New spam email"
+            ]
         }
+        this.handleClick = this.handleClick.bind(this)
     }
 
     componentDidMount() {
@@ -24,8 +30,19 @@ class App extends Component {
         }, 1500);
     }
 
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                isLoggedIn: !prevState.isLoggedIn
+            }
+        })
+    }
+
     render() {
         let wordDisplay = this.state.isLoggedIn ? "in" : "out";
+        
+        let btntext = this.state.isLoggedIn === true ? "LOG OUT" : "LOG IN"
+        let displayText = this.state.isLoggedIn === true ? "Logged in" : "Logged out"
 
         return (
             <div>
@@ -37,10 +54,26 @@ class App extends Component {
                     <h4>and is currently logged {wordDisplay}.</h4>
                 </div>
 
-                {this.state.isLoading ?
-                    <h1>Loading...</h1> : 
-                    <Conditional />
+                <div className="card">
+                    {
+                        this.state.isLoading ?
+                        <h1>Loading...</h1> : 
+                        <Conditional />
+                    }
+                </div>
+
+                {
+                    this.state.unreadMessages.length > 0 &&
+                    <div className="card">
+                        <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+                    </div>
                 }
+
+                <div className="card">
+                    <h3>{displayText}</h3>
+                    <button onClick={this.handleClick}>{btntext}</button>
+                </div>
+
                 <Footer />
             </div>
         )
---
+++ src/components/LoginBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function LoginBtn() {
+    return (
+       <div>
+       </div>
+    )
+}
+
+export default LoginBtn;
