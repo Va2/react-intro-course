d2816062e2c51b8940fd20482f4977f2d21cf8da
Va2 <vadim.brieven@gmail.com>
2019-05-22 14:44:16 +0200

    Lifecycle Methods: Part 2

--- src/App.js
+++ src/App.js
@@ -14,20 +14,39 @@ class App extends Component {
         }
     }
 
+    // UNSAFE_componentWillMount() {
+        // => DEPRECIATED: add prefix (UNSAFE_) from v17.x.x
+    // }
+
     componentDidMount() {
         // GET the data I need to correctly display
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.whatever !== this.props.whatever) {
-            // do something important here
-        }
+    // UNSAFE_componentWillReceiveProps(nextProps) {
+    //     if (nextProps.whatever !== this.props.whatever) {
+    //         // do something important here
+    //     }
+    // => DEPRECIATED: add prefix (UNSAFE_) from v17.x.x
+    // }
+
+    static getDerivedStateFromProps() { // https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops
+        // return the new, updated state based upon the props
+        // It is for the rare cases when your components need to takes incomming props it's receiving from it's parent and set it's own state based upon those props.
+    }
+
+    getSnapshotBeforeUpdate() {
+        // create a backup of the current way thongs are
+        // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
     }
 
     shouldComponentUpdate(nextProps, nextState) {
         // return true if want it to update
         // return false if not
     }
+
+    // UNSAFE_componentWillUpdate() {
+        // => DEPRECIATED: add prefix (UNSAFE_) from v17.x.x
+    // }
 
     componentWillUnmount() {
         // teardown or cleanup your code before your component disappears
