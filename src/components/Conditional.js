19733b1df7a94c7542b5e66f0a286c830761e522
Va2 <vadim.brieven@gmail.com>
2019-05-22 15:50:12 +0200

    Conditional rendering - v3: ternary operator

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 function Conditional(props) {
-    if (props.isLoading === true) {
-        return (
-            <h1>Loading...</h1>
-        )
-    }
     return (
-        <h1>Some cool stuff about conditional rendering</h1>
+        <div>
+            {props.isLoading === true ? <h1>Loading...</h1> : <h1>Some cool stuff about conditional rendering</h1>}
+        </div>
     )
 }
 
